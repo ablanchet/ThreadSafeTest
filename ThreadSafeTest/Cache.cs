fa4f234271fbd4df6e00a14ea4296fa7e55c03d5
Antoine Blanchet <antoine.blanchet@invenietis.com>
2013-05-18 00:05:01 +0200

    First commit

---
+++ ThreadSafeTest/Cache.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadSafeTest
+{
+    public class Cache
+    {
+        readonly string _cachePath;
+
+        #region Singleton business
+        private Cache()
+        {
+            if( !Directory.Exists( Configuration.ResourceDirectory ) )
+            {
+                Directory.CreateDirectory( Configuration.ResourceDirectory );
+            }
+            _cachePath = Path.Combine( Configuration.ResourceDirectory, Configuration.CacheFileName );
+        }
+
+        // type initializer
+        static Cache()
+        {
+            Instance = new Cache();
+        }
+
+        public static Cache Instance { get; private set; }
+        #endregion
+
+        public void CleanCache()
+        {
+            // double check locking around
+            // the cache deletion
+            if( IsCacheReady() )
+            {
+                lock( Instance )
+                {
+                    if( IsCacheReady() )
+                    {
+                        Thread.Sleep( 2000 );
+                        File.Delete( _cachePath );
+                        Console.WriteLine( "Cache cleaned" );
+                    }
+                }
+            }
+        }
+
+        public string GetCacheContent( FakeDatabase database )
+        {
+            // double check locking around 
+            // the cache building
+            if( !IsCacheReady() )
+            {
+                lock( Instance )
+                {
+                    if( !IsCacheReady() )
+                    {
+                        BuildCache( database );
+                    }
+                }
+            }
+            // now we are sure the cache exists
+            // then let the thread access the cache 
+            return AccessCache();
+        }
+
+        bool IsCacheReady()
+        {
+            return File.Exists( _cachePath );
+        }
+
+        void BuildCache( FakeDatabase database )
+        {
+            Thread.Sleep( 5000 );
+            using( var stream = File.CreateText( _cachePath ) )
+            {
+                foreach( string data in database.GetData() ) stream.Write( data + " " );
+            }
+            Console.WriteLine( "Cache built" );
+        }
+
+        string AccessCache()
+        {
+            Console.WriteLine( "Cache accessed" );
+            return File.ReadAllText( _cachePath );
+        }
+    }
+}
---
+++ ThreadSafeTest/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadSafeTest
+{
+    public static class Configuration
+    {
+        public static string ResourceDirectory { get { return "resourcedir"; } }
+
+        public static string CacheFileName { get { return "cache.txt"; } }
+    }
+
+    public class FakeDatabase
+    {
+        public IEnumerable<string> GetData()
+        {
+            yield return "data1";
+            yield return "data2";
+            yield return "data3";
+            yield return "data4";
+            yield return "data5";
+        }
+    }
+
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            FakeDatabase database = new FakeDatabase();
+
+            Task.Factory.StartNew( () =>
+            {
+                Console.WriteLine( "Task 1 asking to get the cache" );
+                Console.WriteLine( "Content 1 :" + Cache.Instance.GetCacheContent( database ) );
+            } );
+            Task.Factory.StartNew( () =>
+            {
+                Console.WriteLine( "Task 2 asking to get the cache" );
+                Console.WriteLine( "Content 2 :" + Cache.Instance.GetCacheContent( database ) );
+            } );
+
+            Thread.Sleep( 200 );
+            
+            Task.Factory.StartNew( () =>
+            {
+                Console.WriteLine( "Task asking to clean the cache" );
+                Cache.Instance.CleanCache();
+            } );
+
+            Console.Read();
+        }
+    }
+}
